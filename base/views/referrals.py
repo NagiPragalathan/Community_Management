f6156c61b0d9ab59e14bbf838b8d412014ed8e6c
Rajan-007 <aravinthrajan034@gmail.com>
2024-06-22 22:58:27 +0530

    sql

--- base/views/referrals.py
+++ base/views/referrals.py
@@ -1,39 +1,43 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from base.models import Referral
-from base.forms import ReferralForm
+from base.models import ReferralSlip, Member
+from django.urls import path
 
-def referral_list(request):
-    referrals = Referral.objects.all()
-    return render(request, 'referrals/referral_list.html', {'referrals': referrals})
-
-def referral_detail(request, pk):
-    referral = get_object_or_404(Referral, pk=pk)
-    return render(request, 'referrals/referral_detail.html', {'referral': referral})
 
 @login_required
-def referral_create(request):
-    if request.method == "POST":
-        form = ReferralForm(request.POST)
-        if form.is_valid():
-            referral = form.save(commit=False)
-            referral.user = request.user  # Assign the current user as the referrer
-            referral.save()
-            return redirect('referral_detail', pk=referral.pk)
-    else:
-        form = ReferralForm()
-    return render(request, 'referrals/referral_form.html', {'form': form})
+def create_referral(request):
+    if request.method == 'POST':
+        # Extract form data
+        to_member_id = request.POST.get('to_member')
+        referral_description = request.POST.get('referral')
+        referral_type = request.POST.get('referral_type')
+        referral_status = request.POST.get('referral_status')
+        address = request.POST.get('address', '')
+        telephone = request.POST.get('telephone')
+        email = request.POST.get('email', '')
+        comments = request.POST.get('comments', '')
+        referral_heat = request.POST.get('referral_heat')
+
+        # Create referral slip
+        ReferralSlip.objects.create(
+            from_user=request.user,
+            to_member_id=to_member_id,
+            referral_description=referral_description,
+            referral_type=referral_type,
+            referral_status=referral_status,
+            address=address,
+            telephone=telephone,
+            email=email,
+            comments=comments,
+            referral_heat=referral_heat
+        )
+        return redirect('referrals/list_referrals')
+
+    # If GET request or other, just show the form
+    members = Member.objects.all()  # Assuming Member model stores chapter members
+    return render(request, 'referrals/create_referral.html', {'members': members})
 
 @login_required
-def referral_edit(request, pk):
-    referral = get_object_or_404(Referral, pk=pk)
-    if request.method == "POST":
-        form = ReferralForm(request.POST, instance=referral)
-        if form.is_valid():
-            referral = form.save(commit=False)
-            referral.user = request.user  # Re-assign the user in case it’s necessary
-            referral.save()
-            return redirect('referral_detail', pk=referral.pk)
-    else:
-        form = ReferralForm(instance=referral)
-    return render(request, 'referrals/referral_form.html', {'form': form})
+def list_referrals(request):
+    referrals = ReferralSlip.objects.filter(from_user=request.user)
+    return render(request, 'referrals/list_referrals.html', {'referrals': referrals})
