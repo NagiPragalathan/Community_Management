c7ee018c051932cf39ac1ddf6305de45b25fa5af
NagiPragalathan <nagipragalathan@gmail.com>
2024-07-07 13:26:48 +0530

    Revert ""Updated common_data view to retrieve user profile image, added conditional image display in base.html template, and modified login redirect logic.""
    This reverts commit 236c4627c5f7195092f22bedb1b99f781eea9d43.

--- base/views/common.py
+++ base/views/common.py
@@ -51,10 +51,6 @@ def common_data(request):
         current_user = request.user
         usr_name = current_user.username
         profile = MainProfile.objects.get(user=current_user)
-        try:
-            usr_image = UserProfile.objects.get(user=current_user).profile_image
-        except:
-            usr_image = "none"
         unseen_messages = oneToOneMessage.objects.filter(receiver=current_user, seen=False)
         unseen_messages_count = unseen_messages.count()
 
@@ -75,10 +71,13 @@ def common_data(request):
             'usr_name': usr_name,
             'unseen_messages_count': unseen_messages_count,
             'unseen_messages': unseen_messages_list,
-            'usr_image':usr_image
         })
     else:
-        return redirect("login")
+        context.update({
+            'usr_name': "No name",
+            'unseen_messages_count': 0,
+            'unseen_messages': [],
+        })
 
     return context
 
