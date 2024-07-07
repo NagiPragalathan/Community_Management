69627db18db27d35feac9ea77cce486da4fe09f5
NagiPragalathan <nagipragalathan@gmail.com>
2024-07-07 14:50:40 +0530

    renewal  functions are updated.. ! "Updated common.py to add renewal date check and calculation, and modified base.html and dashboard.html templates to display renewal message and days left."

--- base/views/common.py
+++ base/views/common.py
@@ -5,7 +5,7 @@ from base.form.forms import CityDataForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 def index(request):
     if request.user.is_authenticated:
@@ -60,24 +60,38 @@ def common_data(request):
             message.display = MainProfile.objects.get(user=message.sender).display_name
             try:
                 message.image = UserProfile.objects.get(user=message.sender).profile_image
-            except:
+            except UserProfile.DoesNotExist:
                 message.image = "none"
-                
             unseen_messages_list.append(message)
-            print(message)
+
+        # Check if the renewal date is expired and calculate days left
+        renewal_message = 0
+        days_left_for_renewal = 0
+        if profile.renewal_due_date:
+            current_date = timezone.now().date()
+            renewal_date = profile.renewal_due_date
+
+            if current_date > renewal_date:
+                renewal_message = 1
+                days_left_for_renewal = (current_date - renewal_date).days * -1  # Negative days to indicate expiration
+            else:
+                days_left_for_renewal = (renewal_date - current_date).days
 
         context.update({
             'usr_profile': profile,
             'usr_name': usr_name,
             'unseen_messages_count': unseen_messages_count,
             'unseen_messages': unseen_messages_list,
+            'renewal_message': renewal_message,  # Add renewal message to context
+            'days_left_for_renewal': days_left_for_renewal,  # Add days left to context
         })
     else:
         context.update({
             'usr_name': "No name",
             'unseen_messages_count': 0,
             'unseen_messages': [],
+            'renewal_message': 0,  # No renewal message for unauthenticated users
+            'days_left_for_renewal': 0,  # No days left calculation for unauthenticated users
         })
 
     return context
-
